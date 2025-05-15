e9830faa642523616a30856aafc91f13c862007b
Viznom <vinson040909@gmail.com>
2025-05-15 13:24:34 -0400

    A nearly-impossible game
    I don't recommend trying Good-Bye. I only made it half way through.

--- src/modules/level-loader.js
+++ src/modules/level-loader.js
@@ -1,5 +1,6 @@
 import { blank } from './levels/blank.js';
 import { welcome } from './levels/welcome.js';
+import { goodBye } from './levels/goodBye.js';
 
 /*
     This class is used for loading levels. The loadLevel() function
@@ -10,6 +11,7 @@ class LevelLoader {
     static levels = {
         blank: () => blank(),
         welcome: () => welcome(),
+		goodBye: () => goodBye(),
     };
 
     static loadLevel(levelName) {
