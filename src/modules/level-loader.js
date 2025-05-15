61da8eaf3514162a0ebd71ea44abaf9b374b053a
1tz47ie <starrydetective@gmail.com>
2025-05-15 09:58:15 -0400

    learning about open source
    the fitness gram pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues. the twenty meter pacer test will begin momentarily. line up at the start.

--- src/modules/level-loader.js
+++ src/modules/level-loader.js
@@ -1,5 +1,6 @@
 import { blank } from './levels/blank.js';
 import { welcome } from './levels/welcome.js';
+import { thefitnessgrampacertest } from './levels/thefitnessgrampacertest.js';
 
 /*
     This class is used for loading levels. The loadLevel() function
@@ -10,6 +11,7 @@ class LevelLoader {
     static levels = {
         blank: () => blank(),
         welcome: () => welcome(),
+		thefitnessgrampacertest: () => thefitnessgrampacertest(),
     };
 
     static loadLevel(levelName) {
